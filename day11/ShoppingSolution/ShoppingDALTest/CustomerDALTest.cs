f6b30ae02afcb7bf5a10d93b19abf349175a241e
Manibharathi <manibharathidct@gmail.com>
2024-04-25 12:28:50 +0530

    Add Unit test in Customer repo

---
+++ day11/ShoppingSolution/ShoppingDALTest/CustomerDALTest.cs
@@ -0,0 +1,78 @@
+using ShoppingDALLib;
+using ShoppingModelLib;
+using ShoppingModelLib.Exceptions;
+
+namespace ShoppingDALTest
+{
+    public class Tests
+    {
+        IRepository<int, Customer> customerRepository;
+        [SetUp]
+        public void Setup()
+        {
+            customerRepository = new CustomerRepository();
+            customerRepository.Add(new Customer { Id = 1, Name = "John" });
+        }
+
+        [Test]
+        public void AddCustomerTest()
+        {
+            Customer customer = new Customer { Id = 2, Name = "Jane" };
+            var result = customerRepository.Add(customer);
+            Assert.AreEqual(customer, result);
+        }
+
+        [Test]
+        public void GetAllCustomersTest()
+        {
+            customerRepository.Add(new Customer { Id = 2, Name = "Jane" });
+            List<Customer> customers = (List<Customer>)customerRepository.GetAll();
+            Assert.AreEqual(2, customers.Count);
+        }
+        [Test]
+
+        public void GetAllCustomersExceptionTest()
+        {
+            Assert.Throws<EmptyDataBaseException>(() => new CustomerRepository().GetAll());
+        }
+
+        [Test]
+        public void GetCustomerByIDTest()
+        {
+            Customer customer = customerRepository.GetByKey(1);
+            Assert.AreEqual("John", customer.Name);
+        }
+        [Test]
+        public void GetCustomerByIDExceptionTest()
+        {
+            Assert.Throws<NoCustomerWithGiveIdException>(() => customerRepository.GetByKey(2));
+        }
+
+        [Test]
+        public void UpdateCustomerTest()
+        {
+            Customer customer = new Customer { Id = 1, Name = "Jane" };
+            var result = customerRepository.Update(customer);
+            Assert.AreEqual(customer, result);
+        }
+        [Test]
+        public void UpdateCustomerExceptionTest()
+        {
+            Customer customer = new Customer { Id = 2, Name = "Jane" };
+            Assert.Throws<NoCustomerWithGiveIdException>(() => customerRepository.Update(customer));
+        }
+
+        [Test]
+        public void DeleteCustomerTest()
+        {
+            Customer customer = customerRepository.Delete(1);
+            Assert.AreEqual("John", customer.Name);
+        }
+
+        [Test]
+        public void DeleteCustomerExceptionTest()
+        {
+            Assert.Throws<NoCustomerWithGiveIdException>(() => customerRepository.Delete(2));
+        }
+    }
+}
