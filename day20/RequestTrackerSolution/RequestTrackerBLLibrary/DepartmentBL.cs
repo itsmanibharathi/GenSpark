cc3f4958e0e76de4d58ff346ed83249f81cfbf67
Manibharathi <manibharathidct@gmail.com>
2024-05-08 14:21:10 +0530

    init requst track app

---
+++ day20/RequestTrackerSolution/RequestTrackerBLLibrary/DepartmentBL.cs
@@ -0,0 +1,80 @@
+﻿using RequestTrackerDALLibrary;
+using RequestTrakerModelLibrary;
+
+namespace RequestTrackerBLLibrary
+{
+    public class DepartmentBL
+    {
+        readonly IRepository<int, Department> _repository;
+        public DepartmentBL()
+        {
+            _repository = new DepartmentRepository();
+        }
+
+        public Department AddDepartment()
+        {
+            try
+            {
+                Department department = new Department();
+                department.BuildDepartmentFromConsole();
+                return _repository.Add(department);
+            }
+            catch (DuplicateDepartmentNameException)
+            {
+                throw new DuplicateDepartmentNameException();
+            }
+        }
+
+        public List<Department> GetAllDepartments()
+        {
+            try
+            {
+                return _repository.GetAll();
+            }
+            catch (EmptyDBException)
+            {
+                throw new EmptyDBException();
+            }
+        }
+        public Department GetDepartment()
+        {
+            try
+            {
+                int id = Department.GetDepartmentIdFromConsole();
+                return _repository.Get(id);
+            }
+            catch (DepartmentNotFoundException)
+            {
+                throw new DepartmentNotFoundException();
+            }
+        }
+        public Department RemoveDepartment()
+        {
+            try
+            {
+                int id = Department.GetDepartmentIdFromConsole();
+                return _repository.Delete(id);
+            }
+            catch (DepartmentNotFoundException)
+            {
+                throw new DepartmentNotFoundException();
+            }
+        }
+
+        public Department UpdateDepartment()
+        {
+            try
+            {
+                Department department = new Department();
+                department.BuildDepartmentFromConsole();
+                return _repository.Update(department);
+            }
+            catch (DepartmentNotFoundException)
+            {
+                throw new DepartmentNotFoundException();
+            }
+        }
+        
+
+    }
+}
---
+++ day20/RequestTrackerSolution/RequestTrackerDALLibrary/UserDefindException.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerDALLibrary
+{
+    public class DuplicateDepartmentNameException : Exception
+    {
+        string msg;
+        public DuplicateDepartmentNameException()
+        {
+            msg = "Department name already exists";
+        }
+        public override string Message => msg;
+    }
+    public class DepartmentNotFoundException : Exception
+    {
+        string msg;
+        public DepartmentNotFoundException()
+        {
+            msg = "Department not found";
+        }
+        public override string Message => msg;
+    }
+    public class EmptyDBException : Exception
+    {
+        string msg;
+        public EmptyDBException()
+        {
+            msg = "Database is empty";
+        }
+        public override string Message => msg;
+    }
+    public class DuplicateEmployeeDetailsException : Exception
+    {
+        string msg;
+        public DuplicateEmployeeDetailsException()
+        {
+            msg = "Employee Details already exists";
+        }
+        public override string Message => msg;
+    }
+
+    public class EmployeeNotFoundException : Exception
+    {
+        string msg;
+        public EmployeeNotFoundException()
+        {
+            msg = "Employee not found";
+        }
+        public override string Message => msg;
+    }
+    public class DuplicateRequestException : Exception
+    {
+        string msg;
+        public DuplicateRequestException()
+        {
+            msg = "Request already exists";
+        }
+        public override string Message => msg;
+    }
+
+    public class RequestIDNotFoundException : Exception
+    {
+        string msg;
+        public RequestIDNotFoundException( )
+        {
+            msg = "Request ID not found";
+        }
+        public override string Message => msg;
+    }
+
+
+}
