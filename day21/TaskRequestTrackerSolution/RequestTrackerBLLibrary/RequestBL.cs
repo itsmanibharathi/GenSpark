960be93eca20aea6d4944b1763b1dbb5d0db6ebe
Manibharathi <manibharathidct@gmail.com>
2024-05-09 18:55:36 +0530

    add RequestTracker

---
+++ day21/TaskRequestTrackerSolution/RequestTrackerBLLibrary/RequestBL.cs
@@ -0,0 +1,34 @@
+﻿using RequestTrackerDALLibrary;
+using RequestTrackerModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerBLLibrary
+{
+    public class RequestBL
+    {
+        private readonly IRepository<int, Request> _repository;
+        public RequestBL()
+        {
+            IRepository<int, Request> repo = new RequestRepositry(new RequestTrackerContext());
+            _repository = repo;
+        }
+
+        public async Task<Request> Add(int empID,string msg)
+        {
+            Request request = new Request() { RequestRaisedBy = empID, RequestMessage = msg };
+            var result = await _repository.Add(request);
+
+            return result;
+        }
+
+        public async Task<IList<Request>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+        
+    }
+}
