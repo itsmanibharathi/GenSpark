5fe7b5b80ec2fb73154a2144bead1742153ee336
Manibharathi <manibharathidct@gmail.com>
2024-05-10 12:38:08 +0530

    init | day22 task

---
+++ day22/TaskRequestTrackerSolution/RequestTrackerDALLibrary/RequestRepositry.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RequestTrackerModelLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerDALLibrary
+{
+    public class RequestRepositry : IRepository<int, Request>
+    {
+        private readonly RequestTrackerContext _context;
+        public RequestRepositry(RequestTrackerContext context)
+        {
+            _context = context;
+        }
+        public async Task<Request> Add(Request entity)
+        {
+            _context.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public Task<Request> Delete(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Request> Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IList<Request>> GetAll()
+        {
+            return await _context.Requests.ToListAsync();
+        }
+
+        public Task<Request> Update(Request entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
