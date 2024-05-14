a61662c4b7618b78b58d51a2270576d104fdaa32
Manibharathi <manibharathidct@gmail.com>
2024-05-14 14:42:12 +0530

    Create Models

---
+++ day24/ClinicSolution/ClinicAPI/Models/Doctor.cs
@@ -0,0 +1,11 @@
+﻿namespace ClinicAPI.Models
+{
+    public class Doctor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfJoin { get; set; }
+        public string Specialization { get; set; }
+
+    }
+}
