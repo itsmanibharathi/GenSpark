37fc7b27ed26c40018f1ae24dbce91468457885b
Manibharathi <manibharathidct@gmail.com>
2024-07-11 09:57:52 +0530

    update python assignment

--- day56_py_Day1/Assignment/q3.py
+++ day56_py_Day1/Assignment/q3.py
@@ -1,4 +1,14 @@
 # 3) Take name and gender print greet with salutation
 
-name = input("What is your name? ")
-print(f"Hello {name}!")
+
+name = input("Enter your name: ")
+gender = input("Enter your gender (M/F): ")
+
+if gender.lower() == 'm':
+    print(f"Hello Mr. {name}")
+
+elif gender.lower() == 'f':
+    print(f"Hello Ms. {name}")
+
+else:
+    print("invalid gender")
--- day56_py_Day1/Assignment/q8.py
+++ day56_py_Day1/Assignment/q8.py
@@ -1,1 +1,7 @@
 # 8) Length of a given input string
+
+def length_of_string(input_string):
+    return len(input_string)
+
+input_string = input("Enter a string: ")
+print(f"Length of the string is: {length_of_string(input_string)}")
